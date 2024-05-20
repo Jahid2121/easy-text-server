f79182b673340c4cc9fb8ffd20fce5848081904e
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-05-20 16:28:15 +0600

    Successfully connected to postgresSQL database

---
+++ db/database.js
@@ -0,0 +1,21 @@
+const { Client } = require('pg')
+
+const client = new Client({
+    host: 'localhost',
+    user: 'postgres',
+    port: 5432,
+    password: '1234',
+    database: 'testDB'
+
+})
+
+client.connect()
+
+client.query(`select * from users`, (err, res) => {
+    if(!err){
+        console.log(res.rows);
+    } else {
+        console.log(err.message);
+    }
+    client.end;
+})
