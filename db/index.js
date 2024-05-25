bce63171a50f886b8b15cee1f023c29cfb7b3bab
Jahid2121 <138579420+Jahid2121@users.noreply.github.com>
2024-05-25 08:02:27 +0600

    Update for deployment

--- db/index.js
+++ db/index.js
@@ -1,23 +1,76 @@
 const { Pool } = require('pg');
-
-const pool = new Pool({
-    host: 'localhost',
-    user: 'postgres',
-    port: 5432,
-    database: 'roomsdb',
-    password: '1234'
-});
-
-
+require('dotenv').config()
 
 // const pool = new Pool({
-//     connectionString: process.env.POSTGRES_URL,
-//   })
+//     host: 'localhost',
+//     user: 'postgres',
+//     port: 5432,
+//     database: 'roomsdb',
+//     password: '1234'
+// });
 
-pool.connect()
-    .then(() => console.log('Connected to the database'))
-    .catch(err => console.error('Connection error', err.stack));
+// pool.connect()
+//     .then(() => console.log('Connected to the database'))
+//     .catch(err => console.error('Connection error', err.stack));
 
-module.exports = pool;
+
+
+
+
+
+const pool = new Pool({
+    connectionString: process.env.POSTGRES_URL,
+    ssl: {rejectUnauthorized: false}
+  })
+
+  const createTables = async () => {
+    try {
+      await pool.query(`
+     CREATE TABLE IF NOT EXISTS rooms (
+    id VARCHAR(255) PRIMARY KEY, 
+    name VARCHAR(35),
+    description VARCHAR(200)
+);
+   `);
+    } catch (err) {
+      console.log(err);
+    }
+  
+    try {
+      await pool.query(`
+      CREATE TABLE IF NOT EXISTS messages (
+        id SERIAL PRIMARY KEY,
+        room_id VARCHAR(100) REFERENCES rooms(id),
+        messageSender VARCHAR(100),
+        content TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    );`
+       )
+    }
+    catch (err) {
+        console.log(err);
+      }
+      try {
+        await pool.query(`
+        CREATE TABLE IF NOT EXISTS blogs (
+            id VARCHAR(255) PRIMARY KEY, 
+            title VARCHAR(255),
+            description VARCHAR(500)
+        );
+        `
+         )
+      }
+      catch (err) {
+          console.log(err);
+        }
+}
+
+createTables()
+
+
+
+module.exports = {
+    query: (text, params) => pool.query(text, params),
+  };
 
 
